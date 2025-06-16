3aaa96945bd0bb0ca3325d23e8ef78aa95ad486c
AdelMej <adelmejrissi@gmail.com>
2025-06-16 15:24:59 +0200

    added a program that print the alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+/**
+* main - print the alphabet without q and e
+*
+* Return: alwayys zero
+*/
+
+int main(void)
+{
+char x = 'a';
+do {
+if(x !='q' && x != 'e'){
+putchar(x);
+} else {
+}
+x++;
+} while (x <= 'z');
+printf("\n");
+return (0);
+}
