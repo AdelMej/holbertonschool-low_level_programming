76776d84b0095d8be199254a0948fa12e5b3813d
AdelMej <adelmejrissi@gmail.com>
2025-06-16 15:51:58 +0200

    remove the executable and added the proper program

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers of base 10
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+printf("%d", x);
+x++;
+}
+printf("\n");
+return (0);
+}
