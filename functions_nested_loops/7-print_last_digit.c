59f37e3e0e9878c857e7bb877b233299526ea05c
AdelMej <adelmejrissi@gmail.com>
2025-06-17 15:22:04 +0200

    fix: canged the function so it works better with negative numbers

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
 * print_last_digit - prints the last digit of a given number
 * @n: the value that will be used to print it's last digit
@@ -10,6 +9,6 @@
 
 int print_last_digit(int n)
 {
-	_putchar((abs(n) % 10) + '0');
-	return (abs(n) % 10);
+	_putchar(abs(n % 10) + '0');
+	return (abs(n % 10));
 }
