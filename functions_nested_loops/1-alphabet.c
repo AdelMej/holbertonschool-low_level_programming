1480ef878235b30c7e787f3b08ff2cd781c1833c
AdelMej <adelmejrissi@gmail.com>
2025-06-17 13:27:38 +0200

    fix: changed the description wasn't correct

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
 * print_alphabet - a function that print the print_alphabet
 *
-* Return: void
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
 
 void print_alphabet(void)
 {
 	char x = 'a';
+
 	while (x <= 'z')
 	{
 		_putchar(x);
