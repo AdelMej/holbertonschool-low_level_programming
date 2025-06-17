cca6f6d89b5db5e1cd6c1be5c818eea8568cb684
AdelMej <adelmejrissi@gmail.com>
2025-06-17 09:19:13 +0200

    feat: a program that prints every single digit with a coma and space between them

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints all comibnations of single digits number seperated with a coma
+*
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+int x = 0;
+do {
+putchar(x + '0');
+if (x != 9)
+{
+putchar(',');
+putchar(' ');
+}
+x++;
+} while (x < 10);
+return (0);
+}
