fb17561a71c7df442e5172f901ab54948747692f
AdelMej <adelmejrissi@gmail.com>
2025-06-18 15:58:06 +0200

    feat: added a program that prints all the combination of 3 digit

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - prints the combination of 3 digit
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+	int x=0;
+	int y=0;
+	int z=0;
+
+	while (z < 10)
+	{
+		while(y < 10)
+		{
+			while (x < 10){
+				if ((z < y) && (y < x))
+				{
+					putchar(z + '0');
+					putchar(y + '0');
+					putchar(x + '0');
+					if(!((z == 7) && (y == 8) && (x == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			x++;
+			}
+			x=0;
+			y++;
+		}
+		y=0;
+		z++;
+	}
+	putchar('\n');
+	return (0);
+}
