26e95e2f0ed8c7034ecdd3e6e607cdf98e6f165d
AdelMej <adelmejrissi@gmail.com>
2025-06-18 13:05:03 +0200

    fix: strlen was not allowed had to fix it

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <string.h>
+
 /**
 * main - print a quote
 *
@@ -9,6 +9,6 @@ int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, strlen(quote));
+	write(2, quote, 59);
 	return (1);
 }
