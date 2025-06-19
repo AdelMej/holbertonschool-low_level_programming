dc5605147b28af5102911871b982d142fe1d0a36
AdelMej <adelmejrissi@gmail.com>
2025-06-19 16:09:00 +0200

    upg: upgrade the function to print table of 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -76,8 +76,16 @@ void printNumber(int n)
 		_putchar(((n / 10) % 10) + '0');
 		_putchar((n % 10) + '0');
 	}
+	else if (n < 10000)
+	{
+		_putchar(((n / 1000) % 10) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
 	else
 	{
+		_putchar(((n / 10000) % 10) + '0');
 		_putchar(((n / 1000) % 10) + '0');
 		_putchar(((n / 100) % 10) + '0');
 		_putchar(((n / 10) % 10) + '0');
