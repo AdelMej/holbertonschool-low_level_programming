d9d3523d0d7e5ebfd82e59dbd063eb44f9c10585
AdelMej <adelmejrissi@gmail.com>
2025-06-20 10:23:22 +0200

    feat: added a script that prints the first 98 fibonnachi numbers

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints the first 98 fibonnachi numbers
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+	double x = 1;
+	double y = 2;
+	double temp = 0;
+	int i;
+
+	for (i = 0; i < 98; i++)
+	{
+		printf("%.f,", x);
+		temp = x;
+		x = y;
+		y = x + temp;
+	}
+	printf("%.f\n", x);
+	return (0);
+}
