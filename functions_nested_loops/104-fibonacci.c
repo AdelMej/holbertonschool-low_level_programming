39492a67247398977d62d0a989160688e727d89e
AdelMej <adelmejrissi@gmail.com>
2025-06-20 11:56:25 +0200

    fix: changed the script by using arrays storing each digit instead

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,87 @@
 #include <stdio.h>
+#define MAXDIGITS 1000
+
+
+/**
+* add - adds the numbers of two given arrays of integer
+* @a: an arrays containing differents digit of a number
+* @b: an array containing different digit of a number
+* @result: the returned value
+*
+* Return: void
+*/
+void add(int a[], int b[], int result[])
+{
+	int carry = 0;
+	int i;
+
+	for (i = 0; i < MAXDIGITS; i++)
+	{
+		int sum = a[i] + b[i] + carry;
+
+		result[i] = sum % 10;
+		carry = sum / 10;
+	}
+}
+
+/**
+* printNumber - print the given number
+* @number: a given array of numbers to be printed
+*
+* Return: void
+*/
+void printNumber(int number[])
+{
+	int i = MAXDIGITS - 1;
+
+	while (i >= 0 && number[i] == 0)
+	{
+		i--;
+	}
+	if (i == -1)
+	{
+		printf("0");
+	}
+	else
+	{
+		for (; i >= 0; i--)
+		{
+			printf("%d", number[i]);
+		}
+	}
+}
 
 /**
 * main - prints the first 98 fibonnachi numbers
 *
 * Return: always zero
 */
-
 int main(void)
 {
-	long double x = 1;
-	long double y = 2;
-	long double temp = 0;
+	int a[MAXDIGITS] = {0};
+	int b[MAXDIGITS] = {1};
+	int c[MAXDIGITS] = {0};
 	int i;
+
+	printNumber(a);
+	printNumber(b);
 
 	for (i = 1; i < 98; i++)
 	{
-		printf("%.Lf, ", x);
-		temp = x;
-		x = y;
-		y = x + temp;
+		int j;
+
+		add(a, b, c);
+		printNumber(c);
+		printf(", ");
+		/* Update a and b for the next iteration */
+		for (j = 0; j < MAXDIGITS; j++)
+		{
+			a[j] = b[j];
+			b[j] = c[j];
+		}
 	}
-	printf("%.Lf\n", x);
+	add(a,b,c);
+	printNumber(c);
+	printf("\n");
 	return (0);
 }
