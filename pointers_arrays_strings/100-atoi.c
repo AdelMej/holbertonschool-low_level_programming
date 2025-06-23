b9a9cb355c977bfe76dc488deac7ba7734d44abe
AdelMej <adelmejrissi@gmail.com>
2025-06-23 19:13:39 +0200

    fix: an issue with signs and incorrect number output

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#ifdef DEBUG_MODE
-	#include <stdio.h>
-#endif
+#include <stdio.h>
 
+#define DEBUG_MODE
 /**
 * _atoi - a function that returns an integer found in a string
 * @s: a string to analyse
@@ -13,42 +12,33 @@
 int _atoi(char *s)
 {
 	int i = 0;
+	int number = 0;
 	int negative = 0;
 	int positive = 0;
-	int number = 0;
-	int nbsize = 0;
-	int multiplier = 1;
 
 	/* placing myself where the number starts */
 	while ((s[i] != '\0') && ((s[i] < '0') || (s[i] > '9')))
-		i++;
-
-	/* calculating how many numbers needs are in the array */
-	while ((s[i + nbsize] != '\0') && ((s[i + nbsize] >= '0')
-		&& (s[i + nbsize] <= '9')))
-		nbsize++;
-
-	/* creating an integer using arithmetic operations */
-	while ((i + nbsize) > i)
 	{
-		number += (s[i - 1 + nbsize] - '0') * multiplier;
-		multiplier *= 10;
-		nbsize--;
+		if(s[i] == '-')
+		{
+			negative++;
+		}
+		else if(s[i] == '+')
+		{
+			positive++;
+		}
+		i++;
 	}
 
-	while ((s[i - 1] == '+') || (s[i - 1] == '-'))
+	/* creating an integer using arithmetic operations */
+	while ((s[i] >= '0') && (s[i] <= '9'))
 	{
-		if(s[i - 1] == '-')
-			negative++;
-		else
-			positive++;
-		i--;
+		number = number * 10 + (s[i] - '0');
+		i++;
 	}
 
 	if (negative > positive)
-	{
 		number *= -1;
-	}
 
 	return (number);
 }
