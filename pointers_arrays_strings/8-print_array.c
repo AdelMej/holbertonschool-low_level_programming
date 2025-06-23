cee93524ade3d0dd82452ee02d4f6ca76fd594f6
AdelMej <adelmejrissi@gmail.com>
2025-06-23 14:51:18 +0200

    fix: changed the syntax for betty standard

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,16 @@
 void print_array(int *a, int n)
 {
 	int i;
- if (n <= 0)
+
+	if (n <= 0)
 	{
 		printf("\n");
 	}
-	else 
+	else
 	{
 		for (i = 0; i <= n - 1; i++)
 		{
-			if(i != n - 1)
+			if (i != n - 1)
 			{
 				printf("%d, ", a[i]);
 			}
