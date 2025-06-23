996ba3695c3ce0219b7e46b1e20fc05363c711c7
AdelMej <adelmejrissi@gmail.com>
2025-06-23 10:40:32 +0200

    feat: added a program tha prints fizz buz depending on multiples of 3 and 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - a program that prints all numbers
+* and fizz and buzz for multiples of 3 and 5
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	do {
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		if (((i % 3) != 0) && ((i % 5 != 0)))
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+		i++;
+	} while (i <= 100);
+	printf("\n");
+	return (0);
+}
