4b76077f80558c047c4a90a957872c0d83047d94
AdelMej <adelmejrissi@gmail.com>
2025-06-24 15:11:03 +0200

    fix: fixed an issue where the program couldn't handle large numbers

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -2,39 +2,29 @@
 
 /**
 * print_number - a
-*
+* @n: a number to print
 *
 *
 */
 
 void print_number(int n)
 {
-	int multi = 1;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
 
-	/* if n is under 10 print n */
-	if (n < 10)
+	if (n == 0)
 	{
-		_putchar(n + '0');
+		_putchar('0');
 	}
 	else
 	{
-		/* finding out the multiple of 10 that can divide n */
-		while (multi < n)
-			multi *= 10;
-		/* dividing by 10 to decrease multiple so that it fit n */
-		multi /= 10;
-
-		/* printing the number */
-		while (multi > 0)
+		if (n / 10)
 		{
-			_putchar(((n / multi) % 10) + '0');
-			multi /= 10;
+			print_number(n / 10);
 		}
+		_putchar(n % 10 + '0');
 	}
 }
