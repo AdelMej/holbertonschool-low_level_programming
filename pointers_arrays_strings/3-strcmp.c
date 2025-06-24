4f4416d2709f766b4532e009a5a3ebd9d38bbc62
AdelMej <adelmejrissi@gmail.com>
2025-06-24 10:31:14 +0200

    feat: added a function that compare 2 given strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if(s1 == s2)
+	int i;
+	int resS1 = 0, resS2 = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		return(0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	else if(s1 > s2)
-	{
-		return (-15);
-	}
-	else 
-	{
-		return(15);
-	}
+
+	return (resS1 - resS2);
 }
