e3ee9ea0e955affbe4215813216d52aad22a658c
AdelMej <adelmejrissi@gmail.com>
2025-06-24 14:53:32 +0200

    fix: changed the syntax for betty standard

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*
+* print_number - a
 *
 *
 *
@@ -10,24 +10,28 @@
 void print_number(int n)
 {
 	int multi = 1;
-	
-	if(n < 0)
+
+	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
 
-	while (multi < n)
-		multi *= 10;
-	multi /= 10;
-
-	if(n < 10)
+	/* if n is under 10 print n */
+	if (n < 10)
 	{
 		_putchar(n + '0');
 	}
-	else 
+	else
 	{
-		while(multi > 0)
+		/* finding out the multiple of 10 that can divide n */
+		while (multi < n)
+			multi *= 10;
+		/* dividing by 10 to decrease multiple so that it fit n */
+		multi /= 10;
+
+		/* printing the number */
+		while (multi > 0)
 		{
 			_putchar(((n / multi) % 10) + '0');
 			multi /= 10;
