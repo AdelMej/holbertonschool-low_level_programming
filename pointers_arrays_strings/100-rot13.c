c108d00a09f036c92b16d205b75bdd8482de1db4
AdelMej <adelmejrissi@gmail.com>
2025-06-25 16:08:43 +0200

    fix: used chatgpt because i couldn't find the logic to use only one loop

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -7,14 +7,30 @@
 * Return: the encoded string
 */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i;
+	char base;
+    char *p = s;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-            str[i] = (str[i] - 'a' + 13) % 26 + 'a';
-	}
-	return (str);
+    while (*p) {
+        /* Only one if statement is allowed */
+        if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) {
+            base = 'A';
+            i = 0;
+
+            /* First loop to determine if lowercase */
+            for (i = 'a'; i <= 'z'; i++) {
+                if (*p == i) {
+                    base = 'a';
+                    break;
+                }
+            }
+
+            /* Apply ROT13 */
+            *p = ((*p - base + 13) % 26) + base;
+        }
+        p++;
+    }
+    return (s);
 }
