22aaf087b6bce633e4bacd5a2d93a1815ee5c280
AdelMej <adelmejrissi@gmail.com>
2025-06-25 16:11:50 +0200

    fix: changed syntax for betty standard

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -2,7 +2,7 @@
 
 /**
 * rot13 - a function that encodes a given string in rot13
-* @str: a given string
+* @s: a given string
 *
 * Return: the encoded string
 */
@@ -11,26 +11,30 @@ char *rot13(char *s)
 {
 	int i;
 	char base;
-    char *p = s;
+	char *p = s;
 
-    while (*p) {
-        /* Only one if statement is allowed */
-        if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) {
-            base = 'A';
-            i = 0;
+	while (*p)
+	{
+		/* Only one if statement is allowed */
+		if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z'))
+		{
+			base = 'A';
+			i = 0;
 
-            /* First loop to determine if lowercase */
-            for (i = 'a'; i <= 'z'; i++) {
-                if (*p == i) {
-                    base = 'a';
-                    break;
-                }
-            }
+			/* First loop to determine if lowercase */
+			for (i = 'a'; i <= 'z'; i++)
+			{
+				if (*p == i)
+				{
+					base = 'a';
+					break;
+				}
+			}
 
-            /* Apply ROT13 */
-            *p = ((*p - base + 13) % 26) + base;
-        }
-        p++;
-    }
-    return (s);
+			/* Apply ROT13 */
+			*p = ((*p - base + 13) % 26) + base;
+		}
+		p++;
+	}
+	return (s);
 }
