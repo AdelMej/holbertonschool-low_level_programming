dcdfafd880b4bfa96a23de709df72dade8967d6b
AdelMej <adelmejrissi@gmail.com>
2025-06-30 17:10:39 +0200

    fix: changed the code it is now clearer and works

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,14 +13,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	if (c == '\0')
-		return (NULL);
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
-			return (&*(s + i));
+			return (s + i);
 	}
 
-	return (NULL);
+	return (s + i);
 }
