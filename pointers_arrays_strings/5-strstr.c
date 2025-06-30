ca12f3004f3e971a0f4bbe7d28c6f179e2010ae3
AdelMej <adelmejrissi@gmail.com>
2025-06-30 19:53:32 +0200

    fix: optimized the code and better readability

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -18,17 +18,18 @@ char *_strstr(char *haystack, char *needle)
 	if (*needle == '\0')
 		return (haystack);
 
-	for (; *haystack != '\0'; haystack++)
+	while (*haystack != '\0')
 	{
 		h = haystack;
 		n = needle;
-		while (*h != '\0' && *n != '\0' && (*h == *n))
+		while (*h && *n && (*h == *n))
 		{
 			h++;
 			n++;
 		}
 		if (*n == '\0')
 			return (haystack);
+		haystack++;
 	}
 
 	return (NULL);
