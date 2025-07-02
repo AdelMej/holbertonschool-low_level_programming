9ac46e6d6a75acafc0df018c89b1498ba0b7d3ca
AdelMej <adelmejrissi@gmail.com>
2025-07-02 11:16:47 +0200

    fix: changed syntax for betty standard

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - 
-*
+* main - a function that add numbers given in parameters
+* @argc: number of parameters
+* @argv: a string containing all the parameters
+* Return: 1 if a non number is given,
+* return 0 if no arguments are given,
+* otherwise return the sum of the parameters
 */
 int main(int argc, char **argv)
 {
 	int i, j;
 	int sum = 0;
 
-	if(argc == 1)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (sum);
 	}
-	
-	for(i = 1; i < argc; i++)
+
+	for (i = 1; i < argc; i++)
 	{
-		for(j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if(argv[i][j] < '0' || argv[i][j] > '9')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
