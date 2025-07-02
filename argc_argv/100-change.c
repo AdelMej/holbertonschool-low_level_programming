daac1f9cee772f010d8b01c0bf1c0db8284a7726
AdelMej <adelmejrissi@gmail.com>
2025-07-02 16:46:00 +0200

    fix: created a function to reduce my main size

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+/* function declaration */
+int isValidNumber(char *s);
+
 /**
-* main - a program that prints the minimum number of changes required
+* main - a program that prints the minimum number of change required
 * @argc: the number of parameters
 * @argv: a string containing all the parameters
 * Return: 0 if sucessful otherwise return 1;
@@ -21,7 +24,8 @@ int main(int argc, char *argv[])
 	}
 	errno = 0;
 	rest = strtol(argv[1], &endPtr, 10); /* convert string to long integer */
-	if (errno == ERANGE || *endPtr != '\0') /* if number is too long */
+	/* if number is too long or is invalid print Error return 0 */
+	if (errno == ERANGE || *endPtr != '\0' || !isValidNumber(argv[1]))
 	{
 		printf("Error\n");
 		return (1);
@@ -41,4 +45,26 @@ int main(int argc, char *argv[])
 	}
 	printf("%ld\n", totalCoins); /* printing the minimum coins needed */
 	return (0);
+}
+
+/**
+* isValidNumber - check if the input is a valid number
+* @s: the string to be tested
+*
+* Return: 1 if it's a number
+* return 0 if it's negative or not a number
+*/
+int isValidNumber(char *s)
+{
+	int i = 0;
+
+	if (s[0] == '-')
+		return (0);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9') /* if it's not a number */
+			return (0);
+	}
+	return (1);
 }
