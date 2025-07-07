223181ad44fc5aaacb8f91baa99192075cc478d8
AdelMej <adelmejrissi@gmail.com>
2025-07-07 11:18:22 +0200

    fix: strlen not allowed so i had to count with a for

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
 * _strdup - a function that copy a string and return it's copy
 * @str: a string to be copied
@@ -11,12 +10,15 @@
 char *_strdup(char *str)
 {
 	char *cpString;
-	size_t i;
+	size_t i, size;
+
+	for (size = 0; str[size] != '\0'; size++)
+		;
 	
 	if(str == NULL)
 		return (NULL);
 
-	cpString = malloc(strlen(str) + 1);
+	cpString = malloc(size + 1);
 	if (cpString == NULL)
 		return (NULL);
 
