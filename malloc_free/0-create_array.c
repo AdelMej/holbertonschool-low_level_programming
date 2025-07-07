8cac34913de0609f2c50594c373e242d3e3e5db8
AdelMej <adelmejrissi@gmail.com>
2025-07-07 10:23:20 +0200

    fix: incorect allocatin size

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,21 +7,22 @@
 * @c: a character for initialization
 *
 * Return: NULL if it failes else the pointer to the string
+* Not: do not forget to free memory
 */
 
 char *create_array(unsigned int size, char c)
 {
 	char *genArr;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
-		return NULL;
-	genArr = malloc((sizeof(char) * size) + 1);
+		return (NULL);
+	genArr = malloc((sizeof(char) * (size + 1)));
 
 	if (genArr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 	{
 		genArr[i] = c;
 	}
