862ded65b625783579f5161de21ffe5b2e8c5eee
AdelMej <adelmejrissi@gmail.com>
2025-07-07 13:02:11 +0200

    fix: changed the logic to deal with empty strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,6 +7,7 @@
 * @s2: a string to concatenate
 *
 * Return: a char
+* Note: the user should free the given pointer
 */
 
 char *str_concat(char *s1, char *s2)
@@ -18,25 +19,20 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if(s2 == NULL)
 		s2 = "";
-
+	if(s1 == NULL && s2 == NULL)
+		return (NULL);
+	/* getting size of s1 and s2 */
 	for (sizeS1 = 0; s1[sizeS1] != '\0'; sizeS1++)
 		;
 	for	(sizeS2 = 0; s2[sizeS2] != '\0'; sizeS2++)
 		;
-
 	concString = malloc(sizeS1 + sizeS2 + 1);
-
 	if (concString == NULL)
 		return (NULL);
-
-	for (i = 0; i < sizeS1; i++)
-	{
+	for (i = 0; i < sizeS1; i++) /* writing s1 */
 		concString[i] = s1[i];
-	}
-	for (i = 0; i < sizeS2; i++)
-	{
+	for (i = 0; i < sizeS2; i++) /* writing s2 */
 		concString[sizeS1 + i] = s2[i];
-	}
 	concString[sizeS1 + i] = '\0';
 	return (concString);
 }
