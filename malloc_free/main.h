dd585ddfa2490f4c5c9e8a1cdb7ff61577f332c0
AdelMej <adelmejrissi@gmail.com>
2025-07-07 10:38:57 +0200

    fix: pushed main.h

---
+++ malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* a function that prints a character */
+int _putchar(char c);
+
+/* a function that creates a string and initialize it's value */
+char *create_array(unsigned int size, char c);
+
+#endif
