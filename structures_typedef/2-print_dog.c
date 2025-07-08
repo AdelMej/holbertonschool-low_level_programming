c1aa104c3c840d4f1ccbfa53ce232dc11a1197ae
AdelMej <adelmejrissi@gmail.com>
2025-07-08 10:33:09 +0200

    feat: added a function that prints a given struture

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,1 +1,28 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_dog - a function that prints a dog structure
+* @d: a structure to be printed
+*
+* Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	char *name, *owner;
+
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			name = "(nil)";
+		else
+			name = d->name;
+		if (d->owner == NULL)
+			owner = "(nil)";
+		else
+			owner = d->owner;
+
+		printf("Name: %s\nAge: %f\nOwner: %s\n", name, d->age, owner);
+	}
+}
