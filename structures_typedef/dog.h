78f195bd945fed8082f5313d267fc98437d10c91
AdelMej <adelmejrissi@gmail.com>
2025-07-08 09:09:52 +0200

    feat: added decoumetation for dog structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog structure
+ * @name: the dog's name
+ * @age: the dog age
+ * @owner: the dog owner
+ *
+ * Description: the structure of a dog containing it's name, age and owner
+ */
 struct dog
 {
 	char *name;
