06e8ed29c8a6afda54daccc127443e075d8c352e
AdelMej <adelmejrissi@gmail.com>
2025-07-14 09:46:14 +0200

    fix: now properly checks for null input

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
-* int_index - a function that compare an array of integer
+* int_index - searches for an integer in an array
 * @array: the array to be compared
 * @size: the size of the array
 * @cmp: the function to use for comparaison
@@ -13,11 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL) /* incorrect size failure */
 		return (-1);
 
 	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
+		if (cmp(array[i])) /* returning index if match is found */
 			return (i);
 
 	return (-1);
