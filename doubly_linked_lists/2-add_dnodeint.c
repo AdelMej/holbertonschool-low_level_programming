6b629f425561c12b985c5d73c605f939f529d87e
AdelMej <adelmejrissi@gmail.com>
2025-07-29 12:03:20 +0200

    fix: betty style error

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
-* add_dnodeint - a function to add a new node at the start of the doubled linked list
+* add_dnodeint - a function to add a new node
+* at the start of the doubled linked list
 * @head: a pointer to the head fo the doubled linked list
 * @n: the value to store in the doubled linked list
 *
@@ -18,8 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	
+
 	new_node->next = *head;
+	new_node->prev = NULL;
+	if (new_node->next != NULL)
+		new_node->next->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
