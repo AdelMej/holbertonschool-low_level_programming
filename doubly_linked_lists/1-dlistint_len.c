870712b5fdbb96887dfef99e2aa413b8a442cf0e
AdelMej <adelmejrissi@gmail.com>
2025-07-29 11:35:17 +0200

    fix: removed a useless tag and fixed grammar

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,18 +2,15 @@
 #include <stddef.h>
 
 /**
-* dlistint_len - a function that returns the length of a doubled linked list
-* @h: the head of the linked list
+* dlistint_len - Returns the number of elements in a doubly linked list
+* @h: Pointer to the head of the list
 *
-* Return: the length of the doubled linked list
+* Return: Number of nodes in the doubly linked list
 */
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *temp;
 	size_t nbNodes = 0;
-
-	if (h == NULL)
-		return (0);
 
 	temp = h;
 	while (temp != NULL)
