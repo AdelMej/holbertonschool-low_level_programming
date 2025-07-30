cecfc0f3a351fe18d92c1efc3b9c2151b2dd98f9
AdelMej <adelmejrissi@gmail.com>
2025-07-30 14:06:24 +0200

    fix: added the prototype for appending text to file function

--- file_io/main.h
+++ file_io/main.h
@@ -8,4 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* a function to create a file with content */
 int create_file(const char *filename, char *text_content);
 
+/* a function that append test to a file */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif
