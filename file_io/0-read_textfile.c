7877d9612430a3fc8acaebf1b565118d25ab43d4
AdelMej <adelmejrissi@gmail.com>
2025-07-30 10:09:07 +0200

    fix: now properly close the file  and free malloc

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,6 +4,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+* read_textfile - read the content of a given file
+* @filename: the name of the file
+* @letters: number of letters to read
+*
+* Return: 0 if fails
+* number of read characters if success
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, nbCharact = 0;
@@ -16,17 +24,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	toWrite = malloc(letters);
+	toWrite = malloc(letters + 1);
 	if (toWrite == NULL)
-		return (-1);
+	{
+		close(fd);
+		return (0);
+	}
 
 	nbCharact = read(fd, toWrite, letters);
 	if (nbCharact == -1)
+	{
+		close(fd);
+		free(toWrite);
 		return (0);
-	
-	nbCharact = write(1, toWrite, letters);
-	if (nbCharact == -1)
-		return (0);
+	}
 
+	nbCharact = write(1, toWrite, nbCharact);
+	if (nbCharact == -1)
+	{
+		close(fd);
+		free(toWrite);
+		return (0);
+	}
+
+	free(toWrite);
 	return (nbCharact);
 }
