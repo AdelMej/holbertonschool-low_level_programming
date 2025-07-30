357795213369f9f4f7d9790c2033aebbc1d35a3a
AdelMej <adelmejrissi@gmail.com>
2025-07-30 18:38:24 +0200

    fix: used flags because of that stupid checker

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,7 +37,8 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fdTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fdTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
