10da456a3550508e9276f331e84232291997fca2
AdelMej <adelmejrissi@gmail.com>
2025-08-01 07:27:44 +0200

    fix: now the permission are fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fdTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
